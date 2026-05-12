9f7276492ceb055933bede5df1c290072d417c92
Josefine <josefine@familienchristensen.com>
2026-05-12 08:30:32 +0200

    Adding cookiecutter.json and {{cookiecutter.project_slug}}

---
+++ {{cookiecutter.project_slug}}/main.py
@@ -0,0 +1,6 @@
+""" This is the main script of the {{ cookiecutter.project_name }}-project.
+    This script is reserved for calling and combining pipelines from the
+        Pipelines/ folders.
+    For a description of the overall functionality of the individual
+        pipelines, please consult their respective Pipelines/README.md.
+"""
---
+++ {{cookiecutter.project_slug}}/test.py
@@ -0,0 +1,7 @@
+""" This is the main test script of the {{ cookiecutter.project_name }}-project.
+    This script is reserved for calling and combining test pipelines 
+        from the Pipelines/*/Tests folders.
+    For a description of the overall functionality of the individual
+        pipeline tests, please consult the "Tests" section of the 
+        respective Pipelines/README.md.
+"""
